00612f6abf86adcb32783210b24aad13a2f6d466
Carlos Sirias <carlos.sirias04@gmail.com>
2023-09-09 19:16:20 -0700

    :tada: first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import '../dist/output.css'
 import confetti from 'canvas-confetti'
 
 const TURNS ={
